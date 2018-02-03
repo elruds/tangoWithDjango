6431132934837e3316ac21e0c63c4e09baf2d29c
elruds <laurenruddocks@gmail.com>
2018-02-03 15:50:12 +0000

    latest

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,8 @@ from rango.forms import CategoryForm
 
 from rango.forms import PageForm
 
+from rango.forms import UserForm, UserProfileForm
+
 def index(request):
 	#return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -85,3 +87,37 @@ def add_page(request, category_name_slug):
 
 	context_dict = {'form':form, 'category':category}
 	return render(request, 'rango/add_page.html', context_dict)
+
+
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+
+			user.set_password(user.password)
+			user.save()
+
+			profile = profile_form.save(commit=False)
+			profile.user = user
+
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
+
+				profile.save()
+
+				registered = True
+
+			else:
+				print(user_form.errors, profile_form.errors)
+
+		else:
+			user_form = UserForm()
+			profile_form = UserProfileForm()
+
+		return render(request, 'rango/register.html', {'user_form':user_form, 'profile_form':profile_form, 'registered':registered})
